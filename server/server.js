4f80909f7e4e63d6f1661fccce4076b6ea043a33
lakshyawork70 <lakshyawork70@gmail.com>
2026-03-13 17:41:32 +0530

    Docker/K8s - f=v6,b=4; ECS f=7,b=5

--- server/server.js
+++ server/server.js
@@ -44,7 +44,11 @@ app.get("/health", (req, res) => {
 });
 
 
-app.listen(port,()=>{
-    console.log(`Server is running on http://localhost:${port}`);
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+// app.listen(port,()=>{
+//     console.log(`Server is running on http://localhost:${port}`);
     
-})
+// })
